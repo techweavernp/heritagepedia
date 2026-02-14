53d7237d09ec8e1413a604d73818925eb61379ab
manohar <mlselegant@gmail.com>
2026-02-14 17:23:13 +0545

    more development

--- database/migrations/2026_02_13_172536_create_heritage_details_table.php
+++ database/migrations/2026_02_13_172536_create_heritage_details_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('audio');
-            $table->json('qlink_tags');
+            $table->string('qlink_tag');
             $table->timestamps();
         });
     }
