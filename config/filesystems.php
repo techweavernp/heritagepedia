9853a45a5914ebb42498875800ee410f92323e2a
manohar <mlselegant@gmail.com>
2026-02-14 18:18:53 +0545

    fx

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'url' => env('ASSET_URL').'/storage',
             'serve' => true,
             'throw' => false,
             'report' => false,
