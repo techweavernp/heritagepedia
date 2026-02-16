6dc1db403383d5b6d7643b8c8896ef8a7a0ac942
manohar <mlselegant@gmail.com>
2026-02-16 18:09:47 +0545

    dashboard widget

---
+++ app/Filament/Widgets/CategoryChart.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\KnowAboutEnum;
+use App\Models\Site;
+use App\Models\UserDetail;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class CategoryChart extends ChartWidget
+{
+    protected ?string $heading = 'Category Chart';
+    protected static ?int $sort = 2;
+    protected static bool $isLazy = true;
+    //protected int | string | array $columnSpan = 'full';
+    protected ?string $maxHeight = '300px';
+
+    protected function getData(): array
+    {
+        $data = Site::join('categories', 'sites.category_id', '=', 'categories.id')
+            ->select('categories.name', DB::raw('count(*) as count'))
+            ->groupBy('categories.name')
+            ->pluck('count', 'categories.name')
+            ->toArray();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Sites',
+                    'data' => array_values($data),
+                    'borderColor' => '#9BD0F5',
+                ],
+            ],
+            'labels' => array_keys($data),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
